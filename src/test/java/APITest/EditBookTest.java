35581ccc45631fbfc3720c4a8cadf984ab7c00a9
aodincov <aodincov@mmtr.ru>
2022-12-16 11:00:49 +0300

    make EditBookTest parametrized

--- src/test/java/APITest/EditBookTest.java
+++ src/test/java/APITest/EditBookTest.java
@@ -2,11 +2,11 @@ package APITest;
 
 import lombok.SneakyThrows;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.resful.booker.APIClients.EndpointProvider;
-import ru.resful.booker.APIClients.booker.DefaultHeaders;
 import ru.resful.booker.auth.UserProvider;
 import ru.resful.booker.auth.Users;
 import ru.resful.booker.clientFactorys.ClientFactory;
@@ -16,23 +16,26 @@ import ru.resful.booker.models.universal.BookingModel;
 import ru.resful.booker.models.responce.WrappedBookingModel;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static ru.resful.booker.APIClients.booker.DefaultHeaders.JSON_HEADER;
 import static ru.resful.booker.APIClients.booker.DefaultHeaders.XML_HEADER;
 
 
 @Tag("API")
-public class EditBookXMLTest {
+public class EditBookTest {
 
     //тут у меня вопрос по jupiter
     //Если в классе будет несколько тестов, то у всех будут свои экземпляры?
-    private final Map<String, String> header = XML_HEADER;
-    EndpointProvider client = ClientFactory.getClient(UserProvider.getUserByName(Users.ADMIN_ROLE),
-            true, ConverterType.XML);
 
     @SneakyThrows
-    @Test
-    public void test() {
+    @ParameterizedTest
+    @MethodSource("testDataProvider")
+    public void test(Map<String, String> header, ConverterType converter) {
+        EndpointProvider client = ClientFactory.getClient(UserProvider.getUserByName(Users.ADMIN_ROLE),
+                true, converter);
+
         WrappedBookingModel oldBook = client.addBook(BookingGenerator.getNewBook(), header).execute().body();
 
         BookingModel newBook = BookingGenerator.getNewBook();
@@ -44,6 +47,13 @@ public class EditBookXMLTest {
         //todo сравнения объектов стоит отдельно вынести и сравнивать по конкретным полям
         assertThat(newBook.getBookingdates(), Matchers.samePropertyValuesAs(editedBook.getBookingdates()));
         assertThat(newBook.setBookingdates(null), Matchers.samePropertyValuesAs(editedBook.setBookingdates(null)));
+    }
+
+    private static Stream<Arguments> testDataProvider(){
+        return Stream.of(
+                Arguments.of(XML_HEADER, ConverterType.XML),
+                Arguments.of(JSON_HEADER, ConverterType.JSON)
+        );
     }
 
 }
